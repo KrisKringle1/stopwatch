c7720ab8fc00e23eb8dc3647c6a18a4c27e52d6c
Kris Ringler <kmringler@gmail.com>
2020-06-04 16:52:57 -0700

    need to figure out onclick of setinterval

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -2,31 +2,36 @@ import React, { useState, useEffect, useRef } from 'react';
 
 const Timer = () => {
   const [count, setCount] = useState(0);
+  const [isRunning, setIsRunning] = useState(false)
+  const [delay, setDelay] = useState(1000);
+
   const savedCallback = useRef();
 
-  function callback() {
-    setCount(count + 1);
+
+  function useInterval(callback, delay) {
+    const savedCallback = useRef();
+
+    useEffect(() => {
+      savedCallback.current = callback;
+    });
+
+    useEffect(() => {
+      function tick() {
+        savedCallback.current();
+      }
+
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }, [delay]);
   }
 
-  useEffect(() => {
-    savedCallback.current = callback;
-  });
-
-  useEffect(() => {
-    function tick() {
-      savedCallback.current();
-    }
-
-    let id = setInterval(tick, 1000);
-    return () => clearInterval(id);
-  }, []);
 
 
 
   return (
     <div>
       <p>{count}</p>
-      <button onClick={callback}>Start</button>
+      <button onClick={useInterval}>Start</button>
       <button >Stop</button>
     </div >
   )
